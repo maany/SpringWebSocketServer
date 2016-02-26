aa1044fdb757503a4647e1d4d8a717d563cc4eec
maany <imptodefeat@gmail.com>
2016-02-26 18:48:51 +0530

    JSON syntax correction

--- src/main/java/hello/TrackDataController.java
+++ src/main/java/hello/TrackDataController.java
@@ -14,10 +14,10 @@ public class TrackDataController {
 
     @MessageMapping("/track")
     @SendTo("/topic/trackdata")
-    public String trackDataHandler(TrackData data) throws Exception {
+    public TrackData trackDataHandler(TrackData data) throws Exception {
         System.out.println("**** Track Data aaya*****");
         System.out.println("POSITIONS : " + data.getPositions());
         // Thread.sleep(3000); // simulated delay
-        return "{'positions' : " + data.getPositions() + "}";
+        return data;
     }
 }
--- src/main/java/hello/WebSocketConfig.java
+++ src/main/java/hello/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		//registry.addEndpoint("/hello").setAllowedOrigins("*").withSockJS();
+		registry.addEndpoint("/hello").setAllowedOrigins("*").withSockJS();
 		registry.addEndpoint("/track").setAllowedOrigins("*").withSockJS();
 	}
 
